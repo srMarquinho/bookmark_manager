f0a006c8e28d8eb758c12db88c25f33cd3d2d350
srMarquinho <sr.marquinho@gmail.com>
2016-06-14 12:38:47 +0100

    step10 - add links/index

--- app.rb
+++ app.rb
@@ -1,8 +1,11 @@
 require 'sinatra/base'
+require './models/link'
 
 class BookmarkManager < Sinatra::Base
-  get '/' do
-    'Hello BookmarkManager!'
+
+  get '/links' do
+    @links = Link.all
+    erb(:'links/index')
   end
 
   # start the server if ruby file executed directly
--- spec/features/see_list_of_links_spec.rb
+++ spec/features/see_list_of_links_spec.rb
@@ -1,15 +1,14 @@
 feature 'Viewing links' do
-  
+
   scenario 'User can see existing links on a page' do
-    
-    Link.create(url: 'http://www.makersacademy.com', title: 'Makersacademy')
+
+    Link.create(url: 'http://www.makersacademy.com', title: 'Makers Academy')
     visit('/links')
     expect(page.status_code).to eq(200)
-    
+
     within 'ul#links' do
       expect(page).to have_content('Makers Academy')
     end
 
   end
-
 end
